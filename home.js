cbf8dd19d05e6cbe20c74a07fc613545ac34d0e5
teba114 <tebas1405@gmail.com>
2024-09-28 04:34:46 +0300

    add variable teast

---
+++ home.js
@@ -0,0 +1,14 @@
+
+function btn1() {
+    document.querySelector(".group-D").style.display = "none";
+    document.querySelector(".group-E").style.display = "inline-block";
+};
+    function btn2() {
+    
+        document.querySelector(".group-E").style.display = "none";
+        document.querySelector(".group-D").style.display = "inline-block";
+        };
+    function Lbtn1() {
+      document.querySelector(".group-E").style.display = "none";
+      document.querySelector(".group-D").style.display = "inline-block";
+    };
